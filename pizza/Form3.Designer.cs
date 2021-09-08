d8fd3fe5075a2f0ab9a31b6974402266206090be
aida curso <aidacurso@hotmail.com>
2021-09-08 16:54:57 -0300

    fggfgfg

--- pizza/Form3.Designer.cs
+++ pizza/Form3.Designer.cs
@@ -43,9 +43,10 @@ namespace pizza
             // 
             // buttonSalvar
             // 
-            this.buttonSalvar.Location = new System.Drawing.Point(131, 133);
+            this.buttonSalvar.Location = new System.Drawing.Point(115, 100);
+            this.buttonSalvar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonSalvar.Name = "buttonSalvar";
-            this.buttonSalvar.Size = new System.Drawing.Size(148, 29);
+            this.buttonSalvar.Size = new System.Drawing.Size(130, 22);
             this.buttonSalvar.TabIndex = 0;
             this.buttonSalvar.Text = "Ver valor total";
             this.buttonSalvar.UseVisualStyleBackColor = true;
@@ -55,18 +56,18 @@ namespace pizza
             // 
             this.labelTitulo.AutoSize = true;
             this.labelTitulo.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.labelTitulo.Location = new System.Drawing.Point(110, 14);
+            this.labelTitulo.Location = new System.Drawing.Point(96, 10);
             this.labelTitulo.Name = "labelTitulo";
-            this.labelTitulo.Size = new System.Drawing.Size(195, 35);
+            this.labelTitulo.Size = new System.Drawing.Size(152, 28);
             this.labelTitulo.TabIndex = 1;
             this.labelTitulo.Text = "Faça seu pedido";
             // 
             // labelDescrição
             // 
             this.labelDescrição.AutoSize = true;
-            this.labelDescrição.Location = new System.Drawing.Point(30, 49);
+            this.labelDescrição.Location = new System.Drawing.Point(26, 37);
             this.labelDescrição.Name = "labelDescrição";
-            this.labelDescrição.Size = new System.Drawing.Size(348, 20);
+            this.labelDescrição.Size = new System.Drawing.Size(270, 15);
             this.labelDescrição.TabIndex = 2;
             this.labelDescrição.Text = "Digite o número referente a pizza que deseja pedir";
             // 
@@ -74,44 +75,48 @@ namespace pizza
             // 
             this.labelValor.AutoSize = true;
             this.labelValor.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.labelValor.Location = new System.Drawing.Point(119, 244);
+            this.labelValor.Location = new System.Drawing.Point(104, 183);
             this.labelValor.Name = "labelValor";
-            this.labelValor.Size = new System.Drawing.Size(172, 23);
+            this.labelValor.Size = new System.Drawing.Size(138, 19);
             this.labelValor.TabIndex = 3;
             this.labelValor.Text = "Valor total do pedido";
             // 
             // textDescricao
             // 
-            this.textDescricao.Location = new System.Drawing.Point(141, 72);
+            this.textDescricao.Location = new System.Drawing.Point(123, 54);
+            this.textDescricao.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textDescricao.Name = "textDescricao";
-            this.textDescricao.Size = new System.Drawing.Size(125, 27);
+            this.textDescricao.Size = new System.Drawing.Size(110, 23);
             this.textDescricao.TabIndex = 4;
             // 
             // textTotal
             // 
             this.textTotal.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.textTotal.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textTotal.Location = new System.Drawing.Point(131, 200);
+            this.textTotal.Location = new System.Drawing.Point(115, 150);
+            this.textTotal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textTotal.Name = "textTotal";
             this.textTotal.ReadOnly = true;
-            this.textTotal.Size = new System.Drawing.Size(148, 41);
+            this.textTotal.Size = new System.Drawing.Size(130, 34);
             this.textTotal.TabIndex = 5;
             this.textTotal.UseWaitCursor = true;
             // 
             // pictureCardapio
             // 
             this.pictureCardapio.Image = ((System.Drawing.Image)(resources.GetObject("pictureCardapio.Image")));
-            this.pictureCardapio.Location = new System.Drawing.Point(396, -517);
+            this.pictureCardapio.Location = new System.Drawing.Point(302, -523);
+            this.pictureCardapio.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureCardapio.Name = "pictureCardapio";
-            this.pictureCardapio.Size = new System.Drawing.Size(807, 1031);
+            this.pictureCardapio.Size = new System.Drawing.Size(718, 1036);
             this.pictureCardapio.TabIndex = 6;
             this.pictureCardapio.TabStop = false;
             // 
             // buttonInformacao
             // 
-            this.buttonInformacao.Location = new System.Drawing.Point(119, 422);
+            this.buttonInformacao.Location = new System.Drawing.Point(104, 316);
+            this.buttonInformacao.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonInformacao.Name = "buttonInformacao";
-            this.buttonInformacao.Size = new System.Drawing.Size(172, 51);
+            this.buttonInformacao.Size = new System.Drawing.Size(150, 38);
             this.buttonInformacao.TabIndex = 7;
             this.buttonInformacao.Text = "Finalizar pedido e verificar informações";
             this.buttonInformacao.UseVisualStyleBackColor = true;
@@ -119,9 +124,9 @@ namespace pizza
             // 
             // Form3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(917, 509);
+            this.ClientSize = new System.Drawing.Size(821, 503);
             this.Controls.Add(this.buttonInformacao);
             this.Controls.Add(this.pictureCardapio);
             this.Controls.Add(this.textTotal);
@@ -130,6 +135,7 @@ namespace pizza
             this.Controls.Add(this.labelDescrição);
             this.Controls.Add(this.labelTitulo);
             this.Controls.Add(this.buttonSalvar);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form3";
             this.Text = "Form3";
             ((System.ComponentModel.ISupportInitialize)(this.pictureCardapio)).EndInit();
